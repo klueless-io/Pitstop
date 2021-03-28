c5bc1d8eb373ecac9693697ae0a691a492ac6430
klueless-io <david@ideasmen.com.au>
2021-03-28 15:37:52 +1100

    update code

--- CustomerManagement.Api.Models/Context/DbPgContext.cs
+++ CustomerManagement.Api.Models/Context/DbPgContext.cs
@@ -1,14 +1,14 @@
 ﻿namespace CustomerManagement.Api.Models.Context
 {
-    using Microsoft.EntityFrameworkCore;
+  using Microsoft.EntityFrameworkCore;
 
-    public class DbPgContext : DomainContext
+  public class DbPgContext : DomainContext
+  {
+    public DbPgContext()
     {
-        public DbPgContext()
-        {
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-                => options.UseNpgsql("Host=127.0.0.1;Port=5432;Database=;Username=postgres;Password=||env DEVELOPER_PASSWORD||");
     }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseNpgsql("Host=127.0.0.1;Port=5432;Database=;Username=postgres;Password=||env DEVELOPER_PASSWORD||");
+  }
 }
